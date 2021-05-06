8a5a04fe333467013a13f0b40fb9e4265d154acf
glennadriangaspar <glennadriangaspar@gmail.com>
2021-05-06 13:35:05 +0800

    initial commit

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', //if class '.courses' if id '#courses' 
+    template: '<h2>Courses</h2>',  
+})
+export class CoursesComponent {
+
+}
